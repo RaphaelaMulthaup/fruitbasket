ce214690ae436e154717486d5b1e1b4680cc6b5d
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-02-11 13:54:51 +0100

    @Input, @Output, ngModel

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SingleFruitComponent } from './single-fruit/single-fruit.component';
 
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SingleFruitComponent],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss',
 })
@@ -85,4 +86,12 @@ export class FruitlistComponent {
       ],
     },
   ];
+
+  addComment(comment:string, index:number){
+    this.fruitlist[index].reviews.push({
+      name: "Raphaela M.",
+      text: comment,
+    })
+  }
+
 }
